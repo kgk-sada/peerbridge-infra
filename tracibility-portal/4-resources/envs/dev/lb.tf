3092463909ae302c38e456ef38620b8db2fba35a
Krikor Garabed Kafalian <krikor.kafalian@sada.com>
2024-06-27 10:59:28 +0400

    yrd

--- tracibility-portal/4-resources/envs/dev/lb.tf
+++ tracibility-portal/4-resources/envs/dev/lb.tf
@@ -6,7 +6,6 @@ module "gce-lb-https" {
   source  = "terraform-google-modules/lb-http/google"
   version = "~> 10.0"
   name    = "xlb-7-https"
-  address = resource.google_compute_global_address.lb.self_link
   project = local.application_project_id
   address = google_compute_global_address.lb.address
   create_address = false
