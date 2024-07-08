71469dad74d78947ad6b76117fa6f9715f0a84de
Krikor Garabed Kafalian <krikor.kafalian@sada.com>
2024-07-08 14:00:18 +0400

    artifact deleted

--- tracibility-portal/1-org/envs/shared/artifact.tf
+++
@@ -1,7 +0,0 @@
-# resource "google_artifact_registry_repository" "traceability_portal_repository" {
-#   location      = "us-central1"
-#   repository_id = "traceability-portal"
-#   description   = "traceability Portal Repository"
-#   format        = "DOCKER"
-#   project = module.devops.project_id
-# }
